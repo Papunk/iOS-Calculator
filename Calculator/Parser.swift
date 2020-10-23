6bb390c98813bbc40d97eb6582b6339ca4f8b74a
Pedro <pedrocpt2001@gmail.com>
2020-10-22 22:10:53 -0400

    Added new class to handle parsing better

---
+++ Calculator/Parser.swift
@@ -0,0 +1,17 @@
+//
+//  Parser.swift
+//  Calculator
+//
+//  Created by Pedro Pagán on 10/22/20.
+//
+
+import Foundation
+
+let mult = { (a: Double, b: Double) -> Double in a * b}
+let div = { (a: Double, b: Double) -> Double in a / b}
+let add = { (a: Double, b: Double) -> Double in a + b}
+let sub = { (a: Double, b: Double) -> Double in a - b}
+
+func getResult(_ a: Double, _ b: Double, _ f: (Double, Double) -> Double) -> Double {
+    return f(a, b)
+}
