cfd54c2c1b4bd1728a3542d2eb25015f080b8113
Pedro <pedrocpt2001@gmail.com>
2020-11-23 11:35:08 -0400

    finished RPN calculator with shunting-yard algorithm

--- Calculator/Parser.swift
+++ Calculator/Parser.swift
@@ -95,16 +95,16 @@ enum Operator: String, CaseIterable {
 }
 
 // Operators:
-let mult = (sign: "×", precedence: 2),
-    div = (sign: "÷", precedence: 2),
-    add = (sign: "+", precedence: 1),
-    sub = (sign: "–", precedence: 1),
-    par = (sign: "(", precedence: 0)
+let mult = (sign: "×", precedence: 2, operation: {(a: Double, b: Double) -> Double in return a * b})
+let div = (sign: "÷", precedence: 2, operation: {(a: Double, b: Double) -> Double in return a / b})
+let add = (sign: "+", precedence: 1, operation: {(a: Double, b: Double) -> Double in return a + b})
+let sub = (sign: "–", precedence: 1, operation: {(a: Double, b: Double) -> Double in return a - b})
+let par = (sign: "(", precedence: 0, operation: /*Placeholder closure*/ {(a: Double, b: Double) -> Double in return 0})
 
-let precedenceList = [mult, div, add, sub, par]
+let operatorList = [mult, div, add, sub, par]
 
 func getPrecedence(_ op: String) -> Int {
-    for type in precedenceList {
+    for type in operatorList {
         if type.sign == op {
             return type.precedence
         }
@@ -112,8 +112,13 @@ func getPrecedence(_ op: String) -> Int {
     return -1
 }
 
-func calculate() {
-    
+func calculate(a: Double, b: Double, op: String) -> Double {
+    for type in operatorList {
+        if type.sign == op {
+            return type.operation(a, b)
+        }
+    }
+    return 0
 }
 
 func tokenize(_ exp: String) -> [String] {
@@ -188,15 +193,17 @@ func turnToRPN(_ exp: [String]) -> [String] {
 func parseRPN(_ exp: [String]) -> Double {
     var answerStack = [Double]()
     
-    for value in exp {
-        if Number.isMember(value) {
-            answerStack.append(Double(value)!)
+    for token in exp {
+        if Number.isMember(token.first!) {
+            answerStack.append(Double(token)!)
         }
-        else if Operator.isMember(value) {
-            
+        else if Operator.isMember(token) {
+            let b = answerStack.popLast()
+            let a = answerStack.popLast()
+            answerStack.append(calculate(a: a!, b: b!, op: token))
         }
     }
     
-    return 0
+    return answerStack.first!
 }
 
